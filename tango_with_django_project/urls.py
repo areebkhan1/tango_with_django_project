e9a899bc8c1fbf1168845bf8e7eed5f7e7c321a4
Areeb Khan <2455329k@student.gla.ac.uk>
2021-01-24 00:32:40 +0000

    Chapter 3 final version with exercises completed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,8 +19,10 @@ from django.urls import include
 from rango import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('rango/', include('rango.urls')),
-	# The above maps any URLs starting with rango/ to be handled by rango
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
+    
+    path('about/', views.about, name='about'),
 ]
